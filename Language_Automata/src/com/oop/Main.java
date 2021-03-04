1a2e80a84c45fdc0429acc2d9a3ad000809906ac
THEKOT001 <70933901+THEKOT001@users.noreply.github.com>
2021-03-04 23:18:03 +0200

    Modified final version
    This is the version that checks every input properly then constructing the language in real time comparing to every terminal symbol on input.

--- Language_Automata/src/com/oop/Main.java
+++ Language_Automata/src/com/oop/Main.java
@@ -1,111 +1,81 @@
 package com.oop;
+
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.*;
 
 
 public class Main {
+    public static Map<String, List<String>> Rules = new HashMap<String, List<String>>();
+
+    String compar = "S";
+    String input = "aabcbb";
+
+
     public static void main(String[] args) {
-        ArrayList<String> finallist = new ArrayList<String>();
-        int limit = 0;
-
-
-        int n = 3;
-
-        int[] arr = new int[n];
-        Random random = new Random();
-        int rand = random.nextInt(2);
-        System.out.println("this is rand =" + rand);
-
-
-        String input = "accb";
-        String compar = "S";
-
-
-        Map<String, List<String>> Rules = new HashMap<String, List<String>>();
         Rules.put("S", new ArrayList<String>(Arrays.asList("aB")));
         Rules.put("B", new ArrayList<String>(Arrays.asList("bB", "cL")));
-        Rules.put("L", new ArrayList<String>(Arrays.asList("cL", "aS", "b")));
+        Rules.put("L", new ArrayList<String>(Arrays.asList("cL", "aS","b")));
 
 
         System.out.println(Arrays.asList(Rules));
-        System.out.println(Rules.values());
-        System.out.println(Arrays.toString(Rules.keySet().toArray()));
+
+        Main main = new Main();
+        main.cycle();
+
+    }
 
 
-         do{
+    public void cycle() {
+        for (int i = 0; i < input.length(); i++) {
+            while (i < input.length()) {
 
-            for (int i = 0; i < input.length(); i++) {
-                int j = random.nextInt(2);
-                int k = random.nextInt(2);
-                int l = random.nextInt(2);
-//                j = arr[0]; k = arr[1]; l = arr[2];
-                System.out.println("j = " + j);
-                System.out.println("k = " + k);
-                System.out.println("l = " + l);
+                if (!input.matches("[abc]+")) {       //look for characters that are NOT a or b or c
+                    System.out.println("Incorrect string");
+                    return;
+                }
 
-                if (Arrays.toString(Rules.keySet().toArray()).contains("S")) {//0
+                if (Arrays.toString(Rules.keySet().toArray()).contains("S")&& compar.contains("S") && input.charAt(i) == 'a') {//0
                     compar = compar.replace("S", "aB");
+                    i++;
+                    System.out.println("\t" + compar);
+
+                }
+                System.out.println("\t" + compar);
+                if (Arrays.toString(Rules.keySet().toArray()).contains("B") && compar.contains("B") && input.charAt(i) == 'b') {//2
+                    compar = compar.replace("B", "bB");
+                    i++;
+                    System.out.println("\t" + compar);
+                } else if(Arrays.toString(Rules.keySet().toArray()).contains("B") && compar.contains("B") && input.charAt(i) == 'c'){
+                    compar = compar.replace("B", "cL");
+                    i++;
                     System.out.println("\t" + compar);
                 }
-                if (Arrays.toString(Rules.keySet().toArray()).contains("B") && j == 1) {        //1
-                    compar = compar.replace("B", "bB");
-                    System.out.println("\t" + compar);
-                } else compar = compar.replace("B", "cL");
-                System.out.println("\t" + compar);
-                if (Arrays.toString(Rules.keySet().toArray()).contains("L") && k == 1) {          //2
+                if (Arrays.toString(Rules.keySet().toArray()).contains("L") && compar.contains("L") && input.charAt(i) == 'c') { //3
                     compar = compar.replace("L", "cL");
+                    i++;
                     System.out.println("\t" + compar);
-                } else if (Arrays.toString(Rules.keySet().toArray()).contains("L") && l == 1) {
+                } else if(Arrays.toString(Rules.keySet().toArray()).contains("L") && compar.contains("L") && input.charAt(i) == 'a'){
                     compar = compar.replace("L", "aS");
-                } else {
-                    compar = compar.replace("L", "b");
+                    i++;
                     System.out.println("\t" + compar);
-
-                    finallist.add(compar);
-
-//                if (compar == finallist.contains(input))
-                    if (compar.equals(input)) {
-                        System.out.println("True");
-                        break;
-
-                    }
-                    if (!(compar.contains("L")) && !(compar.contains("B")) && !(compar.contains("S")) || compar.length() > input.length() + 3) {
-
-                        System.out.println("\t" + "final language for this iteration is " + compar);
-                        limit++;
-                        if (limit > 100) {
-                            break;
-                        }
-                        compar = "S";
-                    } else break;
-
+                }else if(Arrays.toString(Rules.keySet().toArray()).contains("L") && compar.contains("L") && input.charAt(i) == 'b'){
+                    compar = compar.replace("L", "b");
+                    i++;
+                    System.out.println("\t" + compar);
+                }
+                if (compar.endsWith("S") ||  compar.endsWith("B") || compar.endsWith("L")) {
+                    System.out.println("Neterminal at the final.");
+                    return;
                 }
                 if (compar.equals(input)) {
-                    System.out.println("True");
-                    break;
+                    System.out.println("String conforms to the rules.");
+                    return;
+                }else if(i==compar.length()) {
+                    System.out.printf("String unbuildable.");
+                    return;
                 }
-
             }
-//        System.out.println("True " + compar);
-             if (compar.equals(input)) {
-                 System.out.println("True");
-                 break;
-             }
-             if(limit>30){
-                 System.out.println("Parsed 100 times not found  such a language try again or check the introduced language");
-                 break;}
-
-
         }
-         while (compar != input);
     }
 }
-
-
-
-
-
-
-
-

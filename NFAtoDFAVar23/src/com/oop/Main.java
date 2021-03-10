784ae38f8a615d82b5570ed2ab80da5cf166ae7f
THEKOT001 <70933901+THEKOT001@users.noreply.github.com>
2021-03-10 23:12:45 +0200

    Add files via upload

---
+++ NFAtoDFAVar23/src/com/oop/Main.java
@@ -0,0 +1,13 @@
+package com.oop;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        
+        System.out.println("Start node");
+        System.out.println("Final node");
+        System.out.println("Write the rest ");
+    }
+}
